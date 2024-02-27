4e2ae524100272a2d7ffc06692eb14c8a4967c9a
metakat <metakatz@gmail.com>
2024-02-27 18:57:45 +0900

    add logs

--- lib/ret_web/controllers/api/v1/media_controller.ex
+++ lib/ret_web/controllers/api/v1/media_controller.ex
@@ -115,7 +115,7 @@ defmodule RetWeb.Api.V1.MediaController do
   defp resolve_and_render(conn, url, version, quality \\ nil) do
     query = query_for(conn, url, version, quality)
     value = Cachex.fetch(:media_urls, query)
-    Logger.info("Resolved media for #{url} with query #{inspect(query)}")
+    Logger.info(inspect(value))
     maybe_do_telemetry(value)
     maybe_bump_ttl(value, query)
     render_resolved_media_or_error(conn, value)
